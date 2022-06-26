e55ada66cd9846383219df70ed0e73eb2e3329f9
Michael Pingleton <mnpingleton@gmail.com>
2022-06-26 13:35:21 -0400

    Added login and logout functions to api class.

--- src/api/ApiContext.js
+++ src/api/ApiContext.js
@@ -4,6 +4,46 @@ class ApiContext {
         this.accessToken = null;
     }
 
+    login(username, passphrase) {
+        try {
+            fetch('http://localhost:3001/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username,
+                    passphrase: passphrase,
+                }),
+            })
+                .then(response => response.json())
+                .then((data) => {
+                    this.accessToken = data.accessToken;
+                });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    logout() {
+        try {
+            fetch('http://localhost:3001/api/auth/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${this.accessToken}`
+                },
+            })
+                .then(response => response.json())
+                .then((data) => {
+                    this.accessToken = null;
+                });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
 }
 
 export default ApiContext;
