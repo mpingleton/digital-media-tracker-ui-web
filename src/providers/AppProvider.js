a0ac596416ce00a600537524e1830b40a43c5fd7
Michael Pingleton <mnpingleton@gmail.com>
2022-07-01 11:56:16 -0400

    Moved api state inside of the component state.

--- src/providers/AppProvider.js
+++ src/providers/AppProvider.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ApiContext from '../api/ApiContext';
+import login from '../api/calls/login';
 import PrimaryRouter from '../routes/PrimaryRouter';
 
 class AppProvider extends React.Component {
     constructor(props) {
         super(props);
-        this.api = new ApiContext();
         this.state = {
-
+            api: new ApiContext()
         };
     }
 
