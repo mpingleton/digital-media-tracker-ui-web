42550f7b9eaf105625dcfb0cc81b0c4f1d5df4b7
Michael Pingleton <mnpingleton@gmail.com>
2022-07-02 17:31:29 -0400

    Added login and logout functions to API handler.

---
+++ src/api/auth.js
@@ -0,0 +1,32 @@
+
+const login = async (apiContext, loginCredentials, setAccessToken) => {
+    const res = await fetch(apiContext.apiAddr + '/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginCredentials),
+    }).then((data) => data.json());
+
+    if (setAccessToken != undefined) {
+        setAccessToken(res.accessToken);
+    }
+
+    return res;
+};
+
+const logout = async (apiContext) => {
+    const res = await fetch(apiContext.apiAddr + '/api/auth/logout', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${apiContext.accessToken}`,
+        },
+    });
+
+    return res;
+};
+
+module.exports = {
+    login,
+    logout,
+};
