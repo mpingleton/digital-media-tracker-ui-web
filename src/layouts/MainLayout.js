cce380ed5cbc88f1b17889f27da31acaecaecc16
Michael Pingleton <mnpingleton@gmail.com>
2022-07-04 15:10:54 -0400

    Added clickable links and CSS settings to support them.

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -6,10 +6,24 @@ function MainLayout() {
         <div>
             <div className="mainlayout_nav">
                 <div className="mainlayout_nav_left">
-                    <p>Left</p>
+                    <ul className="mainlayout_nav_left">
+                        <li className="mainlayout_nav_left_item">
+                            <a className="mainlayout_nav_left_item" href="/media">Media</a>
+                        </li>
+                        <li className="mainlayout_nav_left_item">
+                            <a className="mainlayout_nav_left_item" href="/about">About</a>
+                        </li>
+                    </ul>
                 </div>
                 <div className="mainlayout_nav_right">
-                    <p>Right</p>
+                    <ul className="mainlayout_nav_right">
+                        <li className="mainlayout_nav_right_item">
+                            <a className="mainlayout_nav_right_item" href="/profile">SrA Doe, John</a>
+                        </li>
+                        <li className="mainlayout_nav_right_item">
+                            <a className="mainlayout_nav_right_item" href="#">Sign Out</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div className="mainlayout_body">
