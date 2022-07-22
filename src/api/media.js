13c9868eab0c2fff356d011362ccaf88c0407b55
Michael Pingleton <mnpingleton@gmail.com>
2022-07-22 03:39:12 +0300

    Added API calls which get media objects by container and facility.

--- src/api/media.js
+++ src/api/media.js
@@ -10,6 +10,32 @@ const getMediaById = async (apiContext, mediaId) => {
     return res;
 };
 
+const getMediaInContainer = async (apiContext, containerId) => {
+    const res = await fetch(apiContext.apiAddr + '/api/media/in/container/' + containerId, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${apiContext.accessToken}`,
+            'Content-Type': 'application/json',
+        },
+    }).then((data) => data.json());
+
+    return res;
+};
+
+const getMediaInFacility = async (apiContext, facilityId) => {
+    const res = await fetch(apiContext.apiAddr + '/api/media/in/facility/' + facilityId, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${apiContext.accessToken}`,
+            'Content-Type': 'application/json',
+        },
+    }).then((data) => data.json());
+
+    return res;
+};
+
 module.exports = {
     getMediaById,
+    getMediaInContainer,
+    getMediaInFacility,
 };
