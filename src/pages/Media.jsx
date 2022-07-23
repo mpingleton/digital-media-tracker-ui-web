5ead32aea6477d7542d6fab2521bb92f6bbb1ec7
Michael Pingleton <mnpingleton@gmail.com>
2022-07-23 10:50:00 +0300

    Added API call for getting media and then display it in a table.

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Media.css';
 
+import {
+    getApiContext,
+    getMediaInFacility,
+} from '../api';
+
 function Media() {
+    const [media, setMedia] = useState({});
+
+    useEffect(() => {
+        getMediaInFacility(getApiContext(), 1).then((data) => setMedia(data));
+    }, []);
+
+    var mediaList = (<tr><td>Loading...</td></tr>);
+    if (media.media !== undefined) {
+        mediaList = media.media.map((m) => (
+            <tr>
+                <td>{m.controlNumber}</td>
+                <td>{m.title}</td>
+                <td>{m.mediaType}</td>
+                <td>{m.classification}</td>
+                <td>{m.lifecycleState}</td>
+            </tr>
+        ));
+    }
+
     return (
         <div className="media_page">
-            <p>Hello world!</p>
+            <table>
+                <tr>
+                    <th>Control Number</th>
+                    <th>Title</th>
+                    <th>Medium</th>
+                    <th>Classification</th>
+                    <th>State</th>
+                </tr>
+                {mediaList}
+            </table>
         </div>
     );
 }
