9e2d18bc5b61a7e7c013c98d64c86df6d767fd4a
Michael Pingleton <mnpingleton@gmail.com>
2022-08-01 20:33:27 +0300

    Added FilterMedia page.

---
+++ src/pages/FilterMedia.jsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react';
+
+import {
+    getApiContext,
+    getFacilitiesInMe,
+} from '../api';
+
+function FilterMedia() {
+    const [facilities, setFacilities] = useState([]);
+
+    useEffect(() => {
+        getFacilitiesInMe(getApiContext()).then((data) => setFacilities(data.facilities));
+    }, []);
+
+    var facilityList = (<h3>Loading...</h3>);
+    if (facilities.length > 0) {
+        facilityList = facilities.map((f) => (
+            <tr>
+                <td>{f.description}</td>
+                <td>{f.buildingAddress}</td>
+                <td>{f.buildingNumber}</td>
+                <td>{f.roomNumber}</td>
+            </tr>
+        ));
+    }
+
+    return (
+        <div className="filter_media_page">
+            <table>
+                <tr>
+                    <th>Description</th>
+                    <th>Address</th>
+                    <th>Building Number</th>
+                    <th>Room Number</th>
+                    <th>Select</th>
+                </tr>
+                {facilityList}
+            </table>
+        </div>
+    );
+}
+
+export default FilterMedia;
--- src/routes/PrimaryRouter.jsx
+++ src/routes/PrimaryRouter.jsx
@@ -8,12 +8,14 @@ import MainLayout from "../layouts/MainLayout";
 import Landing from '../pages/Landing';
 import Login from '../pages/Login';
 import Media from '../pages/Media';
+import FilterMedia from "../pages/FilterMedia";
 
 function PrimaryRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
+                <Route path="/media/filter" element={<MainLayout element={<FilterMedia />} />} />
                 <Route path="/media" element={<MainLayout element={<Media />} />} />
                 <Route path="/" element={<Landing />} />
             </Routes>
