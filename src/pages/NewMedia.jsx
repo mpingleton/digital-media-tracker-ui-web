131b465b6c0bae5d8e5bd7afad94c3acfc1528bd
Michael Pingleton <mnpingleton@gmail.com>
2022-08-03 19:32:19 +0300

    Added form and API call to create media.

--- src/pages/NewMedia.jsx
+++ src/pages/NewMedia.jsx
@@ -6,9 +6,57 @@ import {
 } from '../api';
 
 function NewMedia() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const mediaData = {
+            containerId: Number(event.target.container.value),
+            controlNumber: event.target.controlNumber.value,
+            title: event.target.title.value,
+            mediaType: event.target.mediaType.value,
+            classification: event.target.classification.value,
+            lifecycleState: event.target.lifecycleState.value,
+        };
+
+        createMedia(getApiContext(), mediaData);
+    };
+
     return (
         <div className="new_media_page">
-            
+            <form className="new_media_form" onSubmit={handleSubmit}>
+                <label for="container">Container: </label><br/>
+                <select id="container" name="container">
+                    <option value="1">AA-01 (Facility Name)</option>
+                    <option value="2">AA-02 (Facility Name)</option>
+                </select><br/><br/>
+                <label for="controlNumber">Control Number: </label><br/>
+                <input type="text" id="controlNumber" name="controlNumber" /><br/><br/>
+                <label for="title">Title: </label><br/>
+                <input type="text" id="title" name="title" /><br/><br/>
+                <label for="mediaType">Media Type: </label><br/>
+                <select id="mediaType" name="mediaType">
+                    <option value="CD">CD</option>
+                    <option value="DVD">DVD</option>
+                    <option value="BD">BD</option>
+                    <option value="HDD">HDD</option>
+                    <option value="SSD">SSD</option>
+                </select><br/><br/>
+                <label for="classification">Classification: </label><br/>
+                <select id="classification" name="classification">
+                    <option value="UNCLASSIFIED">Unclassified</option>
+                    <option value="CONFIDENTIAL">Confidential</option>
+                    <option value="SECRET">Secret</option>
+                    <option value="TOP_SECRET">Top Secret</option>
+                </select><br/><br/>
+                <label for="lifecycleState">Lifecycle State: </label><br/>
+                <select id="lifecycleState" name="lifecycleState">
+                    <option value="BLANK">Blank</option>
+                    <option value="ACTIVE">Active</option>
+                    <option value="RELEASED">Released</option>
+                    <option value="DESTROYED">Destroyed</option>
+                </select><br/><br/>
+                <input type="submit" value="Create" />
+            </form>
         </div>
     );
 }
